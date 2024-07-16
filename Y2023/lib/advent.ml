bd6a52af8de26c6bdab2245309ec19a41905d3e8
Chris Mattinson <cmattinson@outlook.com>
2024-07-15 21:08:24 -0600

    chore: remove 2016 and 2023 for now

--- Y2023/lib/advent.ml
+++
@@ -1,3 +0,0 @@
-open Core
-
-let read_lines file = In_channel.with_file file ~f:In_channel.input_lines
--- y2016/lib/advent.ml
+++
@@ -1,5 +0,0 @@
-let read_file (path : string) : string =
-  In_channel.with_open_bin path In_channel.input_all
-;;
-
-
