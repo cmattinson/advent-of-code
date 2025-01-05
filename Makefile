90ccbc9a375c882348d97964e84f0eefbbc1f328
Chris Mattinson <cmattinson@outlook.com>
2025-01-04 20:45:09 -0700

    chore: phony targets in Makefile

--- Makefile
+++ Makefile
@@ -1,5 +1,13 @@
+.PHONY: deps utop test promote
+
 deps:
-	opam install . --deps-only --yes --jobs 8
+	opam install . --deps-only -y -j 8
 
 utop:
 	utop -require core
+
+test:
+	dune test
+
+promote:
+	dune test --auto-promote
--- test/2024/day3_test.ml
+++ test/2024/day3_test.ml
@@ -14,5 +14,5 @@ let%expect_test "sum__enabled_instructions" =
     "%d\n"
     (Day3.sum_enabled_instructions
        "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))");
-  [%expect {| |}]
+  [%expect {| 48 |}]
 ;;
